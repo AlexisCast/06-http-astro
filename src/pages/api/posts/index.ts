ee46b35acb12bfb630767313a726155d71558d0f
Alexis Castellanos <castellanos.alexis@hotmail.com>
2025-09-09 04:00:16 -0600

    feat 3: SSR Adapters

--- src/pages/api/posts/index.ts
+++ src/pages/api/posts/index.ts
@@ -2,11 +2,13 @@
 import type { APIRoute } from "astro";
 import { getCollection } from "astro:content";
 
+export const prerender = false;
 
 
 export const GET: APIRoute = async ({ params, request }) => {
 
   const posts = await getCollection('blog');
+  console.log(request)
 
   return new Response(
     JSON.stringify(posts), {
