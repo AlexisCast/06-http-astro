81fcd139c2a0231f620e988de524902c1cd6d377
Alexis Castellanos <castellanos.alexis@hotmail.com>
2025-09-11 00:43:21 -0600

    feat 10: Clients CRUD

---
+++ src/pages/api/clients/[clientId].ts
@@ -0,0 +1,61 @@
+import type { APIRoute } from "astro";
+
+export const prerender = false;
+
+
+export const GET: APIRoute = async ({ params, request }) => {
+
+  const clientId = params.clientId;
+  const body = {
+    method: 'GET',
+    clientId: clientId
+  }
+
+  return new Response(
+    JSON.stringify(body),
+    {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+  );
+}
+
+export const PATCH: APIRoute = async ({ params, request }) => {
+
+  const clientId = params.clientId;
+  const body = {
+    method: 'PATCH',
+    clientId: clientId
+  }
+
+  return new Response(
+    JSON.stringify(body),
+    {
+      status: 201,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+  );
+}
+
+export const DELETE: APIRoute = async ({ params, request }) => {
+
+  const clientId = params.clientId;
+  const body = {
+    method: 'DELETE',
+    clientId: +clientId!
+  }
+
+  return new Response(
+    JSON.stringify(body),
+    {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+  );
+}
